a78ff8cc40de9c3e6f3097a681e919bde1e6fe8c
rsedxcftvgyhbujnkiqwe <102834306+rsedxcftvgyhbujnkiqwe@users.noreply.github.com>
2024-10-05 16:44:17 -0700

    patch launcher batch file

---
+++ launch_patcher.bat
@@ -0,0 +1,4 @@
+@echo off
+start "" "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf2patcher.exe"
+start "" "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf_win64.exe"
+exit
